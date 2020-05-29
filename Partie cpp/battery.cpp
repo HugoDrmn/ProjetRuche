a0a4e23766536d752c60014e94d7193ae509dfc2
HugoDrmn <45166524+HugoDrmn@users.noreply.github.com>
2020-05-29 19:42:59 +0200

    Add files via upload

---
+++ Partie cpp/battery.cpp
@@ -0,0 +1,115 @@
+/*!
+    @file     battery.cpp
+    @license  BSD (see license.txt)
+    @brief    Programme pour forger l'url des information batterie pour thingspeak
+    @date     06 Avril 2019
+    @version  v1.1 - First release
+    @detail   Compilation  : g++  battery.cpp SimpleIni.cpp i2c.cpp  ina219.cpp -o battery
+              Execution    : ./battery your_write_api_key | envoyerURL
+ */
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+#include <ctime>
+#include <string>
+
+#include "ina219.h"
+using namespace std;
+#include "SimpleIni.h"
+
+#define BATTERY "/opt/Ruche/etc/battery.ini"
+
+/**
+ * @brief ObtenirDateHeure
+ * @return std::string
+ * @details retourne une chaine de caratères représentant la date courante
+ *          au format Année-mois-jour heure:minute:seconde
+ */
+
+string ObtenirDateHeure() {
+    locale::global(locale("fr_FR.utf8"));
+    time_t t = time(0);
+    char Fchar[100];
+    char Tchar[100];
+    string retour;
+    if (strftime(Fchar, sizeof (Fchar), "%F", gmtime(&t))) {
+        if (strftime(Tchar, sizeof (Tchar), "%T", gmtime(&t))) {
+            string F(Fchar);
+            string T(Tchar);
+            retour = F + "%20" + T;
+        }
+    }
+    return retour;
+}
+
+int main(int argc, char *argv[]) {
+    ina219 batterie;
+    ostringstream trame;
+    SimpleIni ini;
+    float charge = 0.0;
+    float capacite;
+    int t0, t1;
+
+    // Lecture du fichier de battery.ini
+    if (!ini.Load(BATTERY)) {
+        return 2;
+    }
+    // derniére capacité en Ah enregistrée
+    charge = ini.GetValue<float>("battery", "charge", 0.0);
+    // Dernier timestamp enregistré
+    t0 = ini.GetValue<int>("battery", "time", 0);
+    // Lecture de la capacité nominale
+    capacite = ini.GetValue<float>("battery", "capacite", 7.0);
+
+    if (argc != 2) {
+        return 1;
+    }
+    if (!batterie.obtenirErreur()) {
+
+        float u = batterie.obtenirTension_V();
+        float i = batterie.obtenirCourantMoyen_A(100);
+        float p = u*i;
+        float z;
+        float soc;
+
+        t1 = time(0);
+        if ((t1 - t0) < 3600) {// deltaT maxi 1 heure utile aprés un long arrèt
+            charge +=z*(t1-t0)/3600 + (i-z)*(t1-t0)/7200;//calcule avec la technique des trapèze
+            z = i;
+        }
+        if (charge < 0) {
+            charge = 0; // La charge ne peut pas être négative
+        }
+
+        soc = (charge / capacite) * 100;
+        if (soc > 100) {
+            soc = 100; // Le SOC maxi est limité à 100%
+        }
+
+        //écriture de la nouvelle charge et de la date de la la dernière relève
+        ini.SetValue<float>("battery", "charge", charge);
+        ini.SetValue<int>("battery", "time", t1);
+
+
+        // le premier argument contient la clé de l'api
+        string key(argv[1]);
+        trame << "https://api.thingspeak.com/update?";
+        trame << "api_key=" << key;
+        trame << "&field1=" << fixed << setprecision(2) << u;
+        trame << "&field2=" << fixed << setprecision(3) << i;
+        trame << "&field3=" << fixed << setprecision(2) << p;
+        trame << "&field4=" << fixed << setprecision(2) << soc;
+        trame << "&field5=" << fixed << setprecision(5) << charge;
+
+        trame << "&created_at=" << ObtenirDateHeure();
+
+        cout << trame.str() << endl;
+    }
+
+    if (!ini.SaveAs(BATTERY)) {
+        return -1;
+    }
+    return 0;
+}
---
+++ Partie cpp/ina219.cpp
@@ -0,0 +1,201 @@
+/*!
+    \file     ina219.cpp
+    \license  BSD (see license.txt)
+    \brief    Classe pour le composant i2c  INA219
+    \detail   Pour comprendre la calibration du INA219
+    	la conversion se fait sur 12 bits (2 exp 12 = 4096)
+    	L'ADC peut mesurer des tensions comprises entre Â± 40mV (Â± pour la mesure du courant bidirectionnel).
+    	le PGA est un diviseur de tension programmable par 1, 2, 4, or 8
+    	Cela donne Ã  l'INA219 une plage effective de Â± 40 mV, Â± 80 mV, Â± 160mV, ou Â± 320mV respectivement.
+    	http://cdwilson.us/articles/understanding-the-INA219
+    \version    1.0 - First release
+*/
+#include "ina219.h"
+#include "i2c.h"
+
+using namespace std;
+
+/*!
+    @brief  Instantiates a new INA219 class
+            and Configures to INA219 to be able to measure up to 32V and 3.2A
+            of current.  Each unit of current corresponds to 100uA, and
+            each unit of power corresponds to 2mW. Counter overflow
+            occurs at 3.2A.
+    @note   These calculations assume a 0.1 ohm resistor is present
+*/
+
+ina219::ina219(int i2cAddress, float  _quantum)
+{
+    deviceI2C = new i2c(i2cAddress);
+    if (!deviceI2C->getError()){
+    	deviceI2C->WriteReg16(REG_CONFIG, 0x0080);   // reset
+
+    	short rc = 4096;
+    	rc = ((rc & 0x00FF) << 8) | ((rc & 0xFF00) >> 8);
+    	deviceI2C->WriteReg16(REG_CALIBRATION , rc);  // écriture du registre de calibration
+
+    	uint16_t config;
+    	config  = BVOLTAGERANGE_32V | GAIN_8 | BADCRES_12BIT | SADCRES_12BIT_128S | MODE_SANDBVOLT_CONTINUOUS;
+    	config = ((config & 0x00FF) << 8) | ((config & 0xFF00) >> 8);  // inversion msb lsb
+    	deviceI2C->WriteReg16(REG_CONFIG, config);
+
+    	quantum = _quantum;
+	error = false;
+     }
+     else{
+	error = true;
+     }
+}
+/*!
+    @brief  destructor
+*/
+
+ina219::~ina219()
+{
+   if (deviceI2C != NULL)
+        delete deviceI2C;
+}
+
+bool ina219::obtenirErreur(){
+	return error;
+    }
+/*!
+    @brief  Set calibration at 16V
+*/
+
+void ina219::fixerCalibration_16V(){
+
+    uint16_t config;
+    config  = BVOLTAGERANGE_16V | GAIN_8 | BADCRES_12BIT | SADCRES_12BIT_128S | MODE_SANDBVOLT_CONTINUOUS;
+    config = ((config & 0x00FF) << 8) | ((config & 0xFF00) >> 8);  // inversion msb lsb
+    deviceI2C->WriteReg16(REG_CONFIG, config);
+}
+
+/*!
+    @brief  Gets the raw bus voltage (float)
+*/
+
+float ina219::obtenirTension_V(){
+
+    float vb;
+    unsigned short data;
+
+    data = deviceI2C->ReadReg16(REG_BUS);
+    data = ((data & 0x00FF) << 8) | ((data & 0xFF00) >> 8); // inversion msb lsb
+    //printf("%d\n",data >> 3); // Ã  décommenter pour  évaluer le quantum expérimentalement
+    vb = (data >> 3) * quantum/1000.0;  // decalage de 3 bits vers la droite 4.000 mv par bit
+    return vb;
+
+}
+
+/*!
+    @brief  Gets the shunt voltage in mili volts
+*/
+
+float ina219::obtenirTensionShunt_mV(){ // la tension aux bornes du shunt 0.01 ohm en mV
+
+    float shuntVoltage;
+    short data,vb;
+
+    data = deviceI2C->ReadReg16(REG_SHUNT);
+    data = ((data & 0x00FF) << 8) | ((data & 0xFF00) >> 8); // inversion msb lsb
+    shuntVoltage = 9.76E-3 * (float)data;   // 9,765625  microV par bit
+    return shuntVoltage;
+}
+
+/*!
+    @brief  Gets the current value in Ampere (float)
+*/
+
+float ina219::obtenirCourant_A(){
+
+    short data;
+    short rc = 4096;
+    rc = ((rc & 0x00FF) << 8) | ((rc & 0xFF00) >> 8);
+    deviceI2C->WriteReg16(REG_CALIBRATION , rc);  // écriture du registre de calibration
+    data = deviceI2C->ReadReg16(REG_CURRENT);
+    data = ((data & 0x00FF) << 8) | ((data & 0xFF00) >> 8); // inversion msb lsb
+    return (float)data * 100E-6;   // 100 micro Ampere par bit
+
+}
+/*!
+    @brief  calcul du courant moyen sur n echantillons prélevés toutes les 10ms
+*/
+float ina219::obtenirCourantMoyen_A(int nb){
+    float som = 0.0;
+    vector<float> valeur;
+
+    for(int i=0; i < nb; i++){
+        valeur.push_back(ina219::obtenirCourant_A());
+        usleep(1000*1);  // attendre 1ms
+    }
+    // Tri des valeurs grÃ¢ce Ã  la fonction std::sort
+    sort (valeur.begin(), valeur.end());
+    // Somme des valeurs sans les deux extrêmes
+    for (int i=2; i < (nb-2) ; i++)
+    {
+        som += valeur.at(i);
+    }
+    return som / (nb - 4);
+}
+
+/*!
+    @brief  Gets the current power in Watt (float)
+*/
+
+
+float ina219::obtenirPuissance_W(){
+
+    short data;
+
+    data = deviceI2C->ReadReg16(REG_POWER);
+    data = ((data & 0x00FF) << 8) | ((data & 0xFF00) >> 8); // inversion msb lsb
+    return (float)data * 2E-3;     // 2 mW par bit
+
+
+}
+
+float ina219::map(float x, float in_min, float in_max, float out_min, float out_max){
+
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+
+}
+
+/*!
+    @brief  Gets the current stat of charge
+    la tension aux bornes d'une batterie diminue ou augmente
+    en fonction de son niveau de charge.
+    La tension sera maximale lorsque la batterie est totalement chargée
+    et minimal lorsquâ€™elle est vide.
+    12.86 -> 100%
+    12.22 -> 50%
+    Les batteries au plomb ont une courbe relativement linéaire,
+    ce qui permet une bonne estimation de l'état de charge : pour une tension mesurée,
+    il est possible d'estimer assez précisément la valeur du SoC associée.
+*/
+
+
+
+float ina219::obtenirSOC(){
+
+     float u = ina219::obtenirTension_V();
+     float i = ina219::obtenirCourantMoyen_A(10);
+     float soc = -1.0;
+     if ( i < 0.0 ){
+     	soc = ina219::map(u, 12.22, 12.86, 50, 100);
+        if (soc > 100) soc = 100;
+        if (soc < 0)   soc = 0;
+     }
+     return soc;
+
+}
+
+/*!
+    @brief  Gets the current version of the class
+*/
+
+void  ina219::version(){
+
+    cout << "\nINA219 PSR Touchard Version 1.0\n" << endl;
+
+}
